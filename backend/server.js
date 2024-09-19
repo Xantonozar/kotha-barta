2b2823c0455cfa5010d776d9d2a34be596b01c49
Ozar <87942063+Xantonozar@users.noreply.github.com>
2024-09-19 08:56:20 +0600

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const axios = require('axios');
 const dummyData = require('./Data/data');
+const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
@@ -24,15 +25,22 @@ app.use('/api/user',UserRouter);
 app.use(bodyParser.json());
 app.use('/api/chat',ChatRoutes);
 app.use('/api/message',MessageRoutes);
+//-------------deployment--------------------
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname1, '/frontend/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname1, 'frontend', 'build', 'index.html'));
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running');
+    });
+}
 app.use(notFound);
 app.use(errorHandler);
 
-app.get('/', (req, res) => {
-    
 
-    
-
-})
 const server = require('http').createServer(app);
  server.listen(5000, () => {
 
